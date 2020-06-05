82989b3c68ed82c4b050acfcec4b0c8f91dbd15e
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-06-05 02:43:57 -0500

    multiple image preview

--- multi-image-upload/src/app/app.component.ts
+++ multi-image-upload/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multi-image-upload';
+  
+  urls = []
+
+  selectedFiles(event){
+    if (event.target.files) {
+      for (var i = 0; i < File.length; i++) {
+        var reader = new FileReader();
+        
+        reader.readAsDataURL(event.target.files[i])
+
+        reader.onload = (event: any) => {
+          this.urls.push(event.target.result);
+        }
+      }
+    }
+  }
 }
